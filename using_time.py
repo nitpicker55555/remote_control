ef16788e6aa11da796b91453d89697f27665e7e1
nitpicker55555 <1303385763@qq.com>
2024-03-16 19:51:46 +0100

    add location + ip and store as json instead of txt

--- using_time.py
+++ using_time.py
@@ -29,19 +29,23 @@ signal.signal(signal.SIGTERM, signal_handler)
 def main():
     response = requests.get("https://ipinfo.io/")
     data = response.json()
+    del data['readme']
 
     current_app = None
     while True:
         try:
             # 获取当前活动窗口
             active_window = gw.getActiveWindow()
+            current_info={}
             if active_window:
                 active_app = active_window._hWnd
                 if current_app != active_app:
                     current_app = active_app
+                    current_info={active_window.title:time.strftime('%Y-%m-%d %H:%M:%S')}
+                    current_info.update(data)
                     with open("windows_record.jsonl", "a", encoding="utf-8") as file:
                         # file.write(f"App {active_window.title} got focus at {time.strftime('%Y-%m-%d %H:%M:%S')}")
-                        file.write(json.dumps({active_window.title:time.strftime('%Y-%m-%d %H:%M:%S')}.update(data)))
+                        file.write(json.dumps(current_info))
                         file.write('\n')
                     print(f"App {active_window.title} got focus at {time.strftime('%Y-%m-%d %H:%M:%S')}")
             time.sleep(1)
